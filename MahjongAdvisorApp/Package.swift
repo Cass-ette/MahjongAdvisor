adea905b471e41230eb819df6b0df9afc1c7f75b
Cass-ette <54059865+Cass-ette@users.noreply.github.com>
2026-06-08 21:59:48 +0800

    chore: scaffold MahjongAdvisorApp package with placeholder

---
+++ MahjongAdvisorApp/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+import PackageDescription
+
+let package = Package(
+    name: "MahjongAdvisorApp",
+    platforms: [.macOS(.v14)],
+    products: [
+        .executable(name: "MahjongAdvisorApp", targets: ["MahjongAdvisorApp"]),
+    ],
+    dependencies: [
+        .package(name: "MahjongCore", path: "../MahjongCore"),
+        .package(name: "MahjongOCR", path: "../MahjongOCR"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "MahjongAdvisorApp",
+            dependencies: [
+                .product(name: "MahjongCore", package: "MahjongCore"),
+                .product(name: "MahjongOCR", package: "MahjongOCR"),
+            ],
+            resources: [.copy("Resources")]
+        ),
+    ]
+)
---
+++ MahjongAdvisorApp/Sources/MahjongAdvisorApp/Placeholder.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+@main
+struct PlaceholderApp {
+    static func main() {
+        print("MahjongAdvisor placeholder")
+    }
+}
